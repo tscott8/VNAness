1bf52075882606ebd6b62aac8ac4b398db7fd594
Rhuarc <ssedrick1@gmail.com>
2016-02-29 09:53:30 -0700

    Rotated and change the controls

--- controls.js
+++ controls.js
@@ -1,23 +1,22 @@
 function expand(percent) {
-    topp.position.y = 5 * percent;
+    topp.position.y = 4 * percent;
     cpu.position.y = 1 * percent;
     gpu.position.y = 1 * percent;
     ram.position.y = 2 * percent;
     ssd.position.y = -1 * percent;
-    battery.position.y = -3 * percent;
-    dvd.position.y = 4 * percent;
-    bus.position.y = -4 * percent;
-    bottom.position.y = -5 * percent;
+    battery.position.y = -2 * percent;
+    dvd.position.y = 3 * percent;
+    bus.position.y = -3 * percent;
+    bottom.position.y = -4 * percent;
 } 
 
 function zoom() {
     
 }
 
-function rotate(degree,laptop) {
-    for (var part in laptop) {
-        part.rotate(degree);
-    }
+function rotate(degree) {
+    laptopFull.rotation.x = degree;
+    laptopFull.rotation.y = degree + Math.PI / 2;
 }
 
 function select(id,laptop) {
